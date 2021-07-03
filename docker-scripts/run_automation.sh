c29471d6d6c1a1c9e24e1ddb5eeb6761a695e644
Frank Ketelaars <fketelaars@nl.ibm.com>
2021-07-03 06:27:22 +0000

    Implement vault subcommand

--- docker-scripts/run_automation.sh
+++ docker-scripts/run_automation.sh
@@ -5,100 +5,100 @@ error=0
 
 # Check mandatory parameters
 if [ ! -v IBM_CLOUD_API_KEY ];then
-    echo "Error: environment variable IBM_CLOUD_API_KEY has not been set and is required"
-    error=1
+  echo "Error: environment variable IBM_CLOUD_API_KEY has not been set and is required"
+  error=1
 fi
 
 if [ -v GIT_REPO_URL ];then
-    if [ ! -v GIT_ACCESS_TOKEN ];then
-        echo "Error: environment variable GIT_ACCESS_TOKEN has not been set and is required"
-        error=1
-    fi
-    if [ ! -v GIT_REPO_DIR ];then
-        echo "Error: environment variable GIT_REPO_DIR has not been set and is required"
-        error=1
-    fi
+  if [ ! -v GIT_ACCESS_TOKEN ];then
+    echo "Error: environment variable GIT_ACCESS_TOKEN has not been set and is required"
+    error=1
+  fi
+  if [ ! -v GIT_REPO_DIR ];then
+    echo "Error: environment variable GIT_REPO_DIR has not been set and is required"
+    error=1
+  fi
 fi
 
 if [ ! -v GIT_REPO_URL ] && [ ! -v CONFIG_DIR ];then
-    echo "Error: either environment variable GIT_REPO_URL or CONFIG_DIR must be specified"
-    error=1
+  echo "Error: either environment variable GIT_REPO_URL or CONFIG_DIR must be specified"
+  error=1
 fi
 
 if [[ error -ne 0 ]];then
-    echo "Error running automation"
-    exit 1
+  echo "Error running automation"
+  exit 1
 fi
 
 # Handle configuration on GitHub
 if [ -v GIT_REPO_URL ];then
-    GIT_DIR=/tmp/automation_config
-    mkdir -p ${GIT_DIR}
+  GIT_DIR=/tmp/automation_config
+  mkdir -p ${GIT_DIR}
 
-    #Disable asking for credentials when login fails.
-    export GIT_TERMINAL_PROMPT=0
+  #Disable asking for credentials when login fails.
+  export GIT_TERMINAL_PROMPT=0
 
-    # Clear any existing content from the CONF_FOLDER_PATH
-    rm -rf ${GIT_DIR}/*
+  # Clear any existing content from the CONF_FOLDER_PATH
+  rm -rf ${GIT_DIR}/*
 
-    # Clone the config repository
-    export GIT_REPO_URL_WITH_TOKEN="https://git_token:${GIT_ACCESS_TOKEN}@${GIT_REPO_URL#https://}"
-    pushd ${GIT_DIR}
-    git clone ${GIT_REPO_URL_WITH_TOKEN} .
-    popd
+  # Clone the config repository
+  export GIT_REPO_URL_WITH_TOKEN="https://git_token:${GIT_ACCESS_TOKEN}@${GIT_REPO_URL#https://}"
+  pushd ${GIT_DIR}
+  git clone ${GIT_REPO_URL_WITH_TOKEN} .
+  popd
 
-    if [ $? -ne 0 ]; then
-        echo "git clone ${GIT_REPO_URL_WITH_TOKEN} failed..."
-        echo "Exiting with exit code 1"
-        echo ""
-        exit 1
-    fi
+  if [ $? -ne 0 ]; then
+    echo "git clone ${GIT_REPO_URL_WITH_TOKEN} failed..."
+    echo "Exiting with exit code 1"
+    echo ""
+    exit 1
+  fi
 
-    CONFIG_DIR=${GIT_DIR}/${GIT_REPO_DIR}
+  CONFIG_DIR=${GIT_DIR}/${GIT_REPO_DIR}
 
-    # Validate if the specified context directory exists
-    if [ ! -d "${CONFIG_DIR}" ]; then
-        echo "Context directory ${GIT_REPO_DIR} not found."
-        echo "Exiting with exit code 1"
-        echo ""
-        exit 1
-    fi
+  # Validate if the specified context directory exists
+  if [ ! -d "${CONFIG_DIR}" ]; then
+    echo "Context directory ${GIT_REPO_DIR} not found."
+    echo "Exiting with exit code 1"
+    echo ""
+    exit 1
+  fi
 
-    # Validate if the configuration directory exists
-    CONF_DIR="${CONFIG_DIR}/config"
-    if [ ! -d "${CONF_DIR}" ]; then
-        echo "config directory not found in context directory ${GIT_REPO_DIR}."
-        echo "Exiting with exit code 1"
-        echo ""
-        exit 1
-    fi
+  # Validate if the configuration directory exists
+  CONF_DIR="${CONFIG_DIR}/config"
+  if [ ! -d "${CONF_DIR}" ]; then
+    echo "config directory not found in context directory ${GIT_REPO_DIR}."
+    echo "Exiting with exit code 1"
+    echo ""
+    exit 1
+  fi
 
-    # Validate if the inventory directory exists
-    INV_DIR="${CONFIG_DIR}/inventory"
-    if [ ! -d "${INV_DIR}" ]; then
-        echo "inventroy directory not found in context directory ${GIT_REPO_DIR}."
-        echo "Exiting with exit code 1"
-        echo ""
-        exit 1
-    fi
+  # Validate if the inventory directory exists
+  INV_DIR="${CONFIG_DIR}/inventory"
+  if [ ! -d "${INV_DIR}" ]; then
+    echo "inventroy directory not found in context directory ${GIT_REPO_DIR}."
+    echo "Exiting with exit code 1"
+    echo ""
+    exit 1
+  fi
 else
-    # Validate if the configuration directory exists
-    CONF_DIR="${CONFIG_DIR}/config"
-    if [ ! -d "${CONF_DIR}" ]; then
-        echo "config directory not found in directory ${CONFIG_DIR}."
-        echo "Exiting with exit code 1"
-        echo ""
-        exit 1
-    fi
+  # Validate if the configuration directory exists
+  CONF_DIR="${CONFIG_DIR}/config"
+  if [ ! -d "${CONF_DIR}" ]; then
+    echo "config directory not found in directory ${CONFIG_DIR}."
+    echo "Exiting with exit code 1"
+    echo ""
+    exit 1
+  fi
 
     # Validate if the inventory directory exists
-    INV_DIR="${CONFIG_DIR}/inventory"
-    if [ ! -d "${INV_DIR}" ]; then
-        echo "inventroy directory not found in directory ${CONFIG_DIR}."
-        echo "Exiting with exit code 1"
-        echo ""
-        exit 1
-    fi
+  INV_DIR="${CONFIG_DIR}/inventory"
+  if [ ! -d "${INV_DIR}" ]; then
+    echo "inventroy directory not found in directory ${CONFIG_DIR}."
+    echo "Exiting with exit code 1"
+    echo ""
+    exit 1
+  fi
 fi
 
 echo ""
@@ -106,12 +106,26 @@ echo "Starting Automation script..."
 echo ""
 cd ${SCRIPT_DIR}/..
 
-ansible-playbook \
+if [ "$SUBCOMMAND" == "environment" ];then
+  ansible-playbook \
     -i ${INV_DIR} \
-    playbook-e2e.yml \
+    playbooks/playbook-e2e.yml \
     --extra-vars config_dir=${CONFIG_DIR} \
     --extra-vars status_dir=${STATUS_DIR} \
     --extra-vars ibmcloud_api_key=${IBM_CLOUD_API_KEY} \
     --extra-vars ibm_cp4d_entitlement_key=${ibm_cp4d_entitlement_key} 
+else if [ "$SUBCOMMAND" == "vault" ];then
+    ansible-playbook \
+    -i ${INV_DIR} \
+    playbooks/playbook-vault.yml \
+    --extra-vars ACTION=${ACTION} \
+    --extra-vars config_dir=${CONFIG_DIR} \
+    --extra-vars status_dir=${STATUS_DIR} \
+    --extra-vars ibmcloud_api_key=${IBM_CLOUD_API_KEY} \
+    --extra-vars secret_group=${VAULT_GROUP} \
+    --extra-vars secret_name=${VAULT_SECRET} \
+    --extra-vars secret_payload=${VAULT_SECRET_VALUE}
+fi
+fi
 
 
