57ab2de44c9567723fe64f21a6894e0ffe2dceb9
Markus Wiegleb <WIEGLEBM@de.ibm.com>
2021-08-18 14:52:58 +0200

    basic attribute modifications working

--- automation-generators/vsi/preprocessor.py
+++ automation-generators/vsi/preprocessor.py
@@ -1,1 +1,21 @@
-print('lol')
+import json
+
+def test():
+    print('this is from test()')
+def preprocessor(attributes=None, full_config=None):
+    result = {
+        attributes_updated: {}
+
+    }
+    #test()
+    # check if the vpc is defined
+    #print(full_config)
+    if 'infrastructure' in attributes:
+        if 'vpc' not in attributes['infrastructure']:
+            # check how many vpcs are defined
+            if len(full_config.get('vpc')) == 1:
+                attributes['infrastructure']['vpc'] = full_config.get('vpc')[0].get('name')
+            else:
+                # add an error to the output
+                
+    return {'attributes_updated': attributes}
--- automation-roles/20-prepare/generators/scripts/pre_execution_processor.py
+++ automation-roles/20-prepare/generators/scripts/pre_execution_processor.py
@@ -1,11 +1,47 @@
-import argparse, json
+import argparse
+#import importlib.util
+import base64, json, yaml
+import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--attributes', '-a', help="attributes for this generator instance", type= str)
-parser.add_argument('--full', '-f', help="merged all_config object containing config for each an every generator instance", type= str)
-parser.add_argument('--preprocessor', '-p', help="path to the preprocessor script we want to include", type= str)
+parser.add_argument('--attributes', '-a', help="attributes for this generator instance", type= str, required=True)
+parser.add_argument('--full', '-f', help="merged all_config object containing config for each an every generator instance", type= str, required=True)
+parser.add_argument('--generatorpath', '-p', help="absolute path to the generator directory", type= str, required=True)
+parser.add_argument('--script', '-s', help="filename of the preprocess-script", type= str, required=True)
 args = parser.parse_args()
 
+generatorAttributes = yaml.load(base64.b64decode(args.attributes), Loader=yaml.FullLoader)
+generatorFullConfig = yaml.load(base64.b64decode(args.full), Loader=yaml.FullLoader)
+
+# add the generators directory to the path 
+# to load definitions from the preprocessor-file
+sys.path.append(args.generatorpath)
+from preprocessor import preprocessor
+
+# result should contain
+# attributes_updated
+# validation.infos
+# validation.warnings
+# validation.errors
+result = preprocessor(attributes=generatorAttributes, full_config=generatorFullConfig)
+
+
+
+print('--- preprocessor result ---')
+print(result)
+#spec = importlib.util.spec_from_file_location("preprocessor", args.generatorpath+'/'+args.script)
+#preprocessor = importlib.util.module_from_spec(spec)
+#spec.loader.exec_module(preprocessor)
+#main('test')
+
 print(json.dumps({
-        'info': args.preprocessor,
+        'input': {
+            'path_to_preprocessor':args.generatorpath,
+            'attributes': generatorAttributes,
+            'full_config': generatorFullConfig
+        },
+        'output': {
+            'attributes_updated': result.get('attributes_updated')
+        }
+
     }, indent=4, separators=(',', ': '), sort_keys=True))
