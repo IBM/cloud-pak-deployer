5bdd87847c6861df50a051c80aa2f375c6d848d1
Markus Wiegleb <WIEGLEBM@de.ibm.com>
2021-09-29 11:28:10 +0200

    checking connections

--- automation-generators/ibm-cloud/transit_gateway/preprocessor.py
+++ automation-generators/ibm-cloud/transit_gateway/preprocessor.py
@@ -3,15 +3,23 @@ from generatorPreProcessor import GeneratorPreProcessor
 def preprocessor(attributes=None, fullConfig=None):
     g = GeneratorPreProcessor(attributes,fullConfig)
 
+    fc = g.getFullConfig()
     tgw = g.getExpandedAttributes()
 
     g('name').isRequired()
     g('location').isRequired()
 
+    vpc_names = []
+    if 'vpc' in fc:
+        vpc_names = fc.match('vpc[*].name')
+
     if 'connections' in tgw:
         for conn in tgw['connections']:
             if 'vpc' not in conn:
                 g.appendError(msg='property vpc must be specified for all elements in list connections')
+            else:
+                if conn['vpc'] not in vpc_names:
+                    g.appendError(msg="'"+conn['vpc']+ "' is not an existing VPC name (Found VPCs: ["+ ','.join(vpc_names) +"] )")
 
 
     result = {
