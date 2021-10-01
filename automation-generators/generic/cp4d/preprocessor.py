39795dc96ad71402fd65b391ad03c1d06bf648fd
Markus Wiegleb <WIEGLEBM@de.ibm.com>
2021-10-01 12:12:27 +0200

    added openshift_storage checks

--- automation-generators/generic/cp4d/preprocessor.py
+++ automation-generators/generic/cp4d/preprocessor.py
@@ -204,9 +204,8 @@ def preprocessor(attributes=None, fullConfig=None):
                     liteFound=True
                 if c['name'] == "cp-foundation":
                     cpFoundationFound=True
-                else:
-                    if "subscription_channel" not in c:
-                        g.appendError(msg='subscription_channel ust be specified for all cartridges, except for cp-foundation')
+                if  (c['name'] != "cp-foundation") and ("subscription_channel" not in c):
+                    g.appendError(msg='subscription_channel ust be specified for all cartridges, except for cp-foundation')
         # iteration over cartridges is done
         # now check if the required fields were found in the 
         # for-loop
@@ -220,9 +219,38 @@ def preprocessor(attributes=None, fullConfig=None):
 # Check reference
 # - Retrieve the openshift element with name=openshift_cluster_name
 # - Within the openshift element retrieve, there must be an openshift_storage element with the name cp4d.openshift_storage_name
+        openshift_names = []
+        if 'openshift' in fc:
+            openshift_names = fc.match('openshift[*].name')
 
+            if 'openshift_cluster_name' in ge:
+                if ge['openshift_cluster_name'] not in openshift_names:
+                    g.appendError(msg="Wasn't able to find a cluster with name:"+ge['openshift_cluster_name']+' ')
 
+                else:
+                    # we made sure the cluster referenced by openshift_cluster_name exists
+                    # now check if it has a openshift_storage with the name cp4d.openshift_storage_name
 
+                    # to make use of benedict .find() we'll need list indexes
+                    # therefore we'll loop over the list indexes and not over the contained objects/entries
+                    for cluster_index in range(len(fc['openshift'])):
+                            # iterate over the openshift clusters
+                            # until we are at the cluster with the name
+                            # referenced in ge['openshift_cluster_name']
+                            if fc['openshift'][cluster_index].get('name',None) == ge['openshift_cluster_name']:
+                                # check if the cluster referenced by ge['openshift_cluster_name'] has a 'openshift_storage'
+                                # attribute defined
+                                if 'openshift_storage' not in fc['openshift['+str(cluster_index)+']']:
+                                    g.appendError(msg="The cluster '"+ ge['openshift_cluster_name'] +"' has no attribute openshift_storage")
+                                else:
+                                    # receive the names of the entries inside the clusters openshift_storage-list
+                                    remote_storage_names = []
+                                    remote_storage_names = fc.match('openshift['+str(cluster_index)+'].openshift_storage[*].storage_name')
+                                    if 'openshift_storage_name' in ge:
+                                        if ge['openshift_storage_name'] not in remote_storage_names:
+                                            g.appendError(msg="The cluster with name "+ ge['openshift_cluster_name'] +" doesn't have a openshift_storage element with name "+ge['openshift_storage_name'] +"")
+
+        
     result = {
         'attributes_updated': g.getExpandedAttributes(),
         'errors': g.getErrors()
