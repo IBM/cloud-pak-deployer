5d6385ff35afb8589deb6fc81275edd467338d29
Markus Wiegleb <WIEGLEBM@de.ibm.com>
2021-10-19 11:14:54 +0200

    new object types

--- automation-generators/ibm-cloud/cos/preprocessor.py
+++ automation-generators/ibm-cloud/cos/preprocessor.py
@@ -4,7 +4,14 @@ def preprocessor(attributes=None, fullConfig=None):
 
     g = GeneratorPreProcessor(attributes,fullConfig)
     g('name').isRequired()
+    g('plan').isRequired()
+    g('location').isRequired()
 
+    ge=g.getExpandedAttributes()
+    if 'buckets' in ge:
+        for bucket in ge['buckets']:
+            if 'name' not in bucket:
+                g.appendError(msg='property name must be specified for all elements in list buckets')
 
     result = {
         'attributes_updated': g.getExpandedAttributes(),
