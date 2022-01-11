85d67a8a010c5e2fd8e47d962b33e521040d34c2
Frances <hjwh@cn.ibm.com>
2022-01-11 21:44:30 +0800

    Update webapp.py
    add api run deploy scripts

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+import subprocess
 
 app = Flask(__name__,static_url_path='', static_folder='ww')
 
@@ -8,6 +9,14 @@ def index():
     return send_from_directory(app.static_folder,'index.html')
 
 
-@app.route('v2/api/deploy')
-def deploy():
-    return
+@app.route('v2/api/deploy',methods=["POST"])
+def deploy(config):
+    process = subprocess.Popen(['cp-deploy.sh', 'env', 'apply','-e env_id=pluto-01','-e ibm_cloud_region=eu-gb'], 
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True,
+                           env={'IBM_CLOUD_API_KEY': '/path/to/directory',
+                                'CP_ENTITLEMENT_KEY':'',
+                                'STATUS_DIR':'',
+                                'CONFIG_DIR':''})
+    
+    return 'runing'
