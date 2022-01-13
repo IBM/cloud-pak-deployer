4ea66237df747515de8a22ca163bbd6075df9c51
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-01-13 20:14:20 +0100

    cp-deploy env command fails

--- cp-deploy.sh
+++ cp-deploy.sh
@@ -144,7 +144,7 @@ environment)
       echo "$ACTION action not allowed when running inside container"
       exit 99
     fi
-    export SUBCOMMAND="command"
+    export ACTION="command"
     shift 1
     ;;
   *)
