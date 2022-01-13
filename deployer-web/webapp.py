bee0fa9fd3901e457d593c2689af0ea08a4e0b83
Frances <hjwh@cn.ibm.com>
2022-01-13 13:40:49 +0800

    Update webapp.py

--- deployer-web/webapp.py
+++ deployer-web/webapp.py
@@ -2,6 +2,7 @@ from flask import Flask, send_from_directory,request
 import json
 import subprocess
 import os
+import yaml
 
 app = Flask(__name__,static_url_path='', static_folder='ww')
 
@@ -28,8 +29,19 @@ def deploy():
                            stdout=subprocess.PIPE,
                            universal_newlines=True,
                            env=env)
-    
     return 'runing'
 
+@app.route('/api/v1/loadConifg',methods=["POST"])
+def loadConfig():
+    body = json.loads(request.get_data())
+    env_id=body['envId']
+    confg_path=body['configDir']
+    result={}
+    result["cp4d"]=open(confg_path+'/cp4d.yaml', "r").read()
+    result["envId"]=open(confg_path+'/config/{}.yaml'.format(env_id), "r").read()
+    return json.dumps(result)
+            
+        
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='32080', debug=False)    
