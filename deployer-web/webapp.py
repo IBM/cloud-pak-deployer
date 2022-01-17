61ee893135317e80a576cef6996a5b59f20da9ba
Frances <hjwh@cn.ibm.com>
2022-01-17 21:22:33 +0800

    Update webapp.py
    fix command line issues

--- deployer-web/webapp.py
+++ deployer-web/webapp.py
@@ -10,15 +10,15 @@ from pathlib import Path
 app = Flask(__name__,static_url_path='', static_folder='ww')
 
 source = os.getcwd()
-#parent = source
-parent = os.path.dirname(source)
+parent = source
+#parent = os.path.dirname(source)
 cp4d_config_path = os.path.join(parent,'sample-configurations/web-ui-base-config/cloud-pak')
 ocp_config_path = os.path.join(parent,'sample-configurations/web-ui-base-config/ocp')
 inventory_config_path = os.path.join(parent,'sample-configurations/web-ui-base-config/inventory')
-confg_dir=str(os.getenv('CONFIG_DIR'))
+config_dir=str(os.getenv('CONFIG_DIR'))
 status_dir=str(os.getenv('STATUS_DIR'))
-target_config=confg_dir+'/config'
-target_inventory=confg_dir+'/inventory'
+target_config=config_dir+'/config'
+target_inventory=config_dir+'/inventory'
 
 Path( status_dir+'/log' ).mkdir( parents=True, exist_ok=True )
 Path( target_config ).mkdir( parents=True, exist_ok=True )
@@ -36,15 +36,14 @@ def deploy():
     if body['cloud']=='ibm-cloud':
       env = {'IBM_CLOUD_API_KEY': body['env']['ibmCloudAPIKey'],
              'CP_ENTITLEMENT_KEY': body['env']['entilementKey'],
-             'CONFIG_DIR':confg_dir,
+             'CONFIG_DIR':config_dir,
              'STATUS_DIR':status_dir}
 
-      process = subprocess.run([parent+'/cp-deploy.sh', 'env', 'apply','-e', 'env_id={}'.
-                        format(body['envId']),'-e' 'ibm_cloud_region={}'.format(body['region']), '--check-only'], 
+      process = subprocess.Popen([parent+'/cp-deploy.sh', 'env', 'apply','-e', 'env_id={}'.
+                        format(body['envId']), '-e', 'ibm_cloud_region={}'.format(body['region']), '--check-only'], 
                     stdout=subprocess.PIPE,
                     universal_newlines=True,
                     env=env)
-
     return 'runing'
 
 @app.route('/api/v1/cartridges/<cloudpak>',methods=["GET"])
