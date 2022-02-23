a5793abe3dca88c65470e0babf0622d1720e9092
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-02-23 17:38:05 +0100

    #410 Check image_registry on IBM Cloud

---
+++ automation-generators/ibm-cloud/image_registry/preprocessor.py
@@ -0,0 +1,29 @@
+from generatorPreProcessor import GeneratorPreProcessor
+import sys
+
+# Validating:
+# image_registry:
+# - name: cpd405
+#   registry_host_name: de.icr.io
+#   registry_namespace: cpd405
+
+def preprocessor(attributes=None, fullConfig=None):
+    g = GeneratorPreProcessor(attributes,fullConfig)
+
+    g('name').isRequired()
+    g('registry_host_name').isRequired()
+    g('registry_namespace').isRequired()
+
+    # Now that we have reached this point, we can check the attribute details if the previous checks passed
+    if len(g.getErrors()) == 0:
+        fc = g.getFullConfig()
+        ge=g.getExpandedAttributes()
+
+        if not ge['registry_host_name'].endswith('.icr.io'):
+            g.appendError(msg='Only IBM Container Registry can be used as a private registry on IBM Cloud. Host name must end with .icr.io')
+
+    result = {
+        'attributes_updated': g.getExpandedAttributes(),
+        'errors': g.getErrors()
+    }
+    return result
