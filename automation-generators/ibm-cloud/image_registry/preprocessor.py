c538cebbe93eeece2df44cb56ae741796ea48439
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-02-24 07:12:35 +0100

    #410 Make messages IBM cloud specific

--- automation-generators/ibm-cloud/image_registry/preprocessor.py
+++ automation-generators/ibm-cloud/image_registry/preprocessor.py
@@ -12,12 +12,14 @@ def preprocessor(attributes=None, fullConfig=None):
 
     g('name').isRequired()
     g('registry_host_name').isRequired()
-    g('registry_namespace').isRequired()
 
     # Now that we have reached this point, we can check the attribute details if the previous checks passed
     if len(g.getErrors()) == 0:
         fc = g.getFullConfig()
         ge=g.getExpandedAttributes()
+
+        if 'registry_namespace' not in ge:
+            g.appendError(msg='Registry namespace is required for IBM Container Registry')
 
         if not ge['registry_host_name'].endswith('.icr.io'):
             g.appendError(msg='Only IBM Container Registry can be used as a private registry on IBM Cloud. Host name must end with .icr.io')
