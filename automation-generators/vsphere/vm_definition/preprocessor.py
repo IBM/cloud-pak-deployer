d407421a247839cb42840535f35254c46e84c101
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-03-31 09:02:38 +0200

    #445 Use vm_definition overrides

---
+++ automation-generators/vsphere/vm_definition/preprocessor.py
@@ -0,0 +1,36 @@
+from generatorPreProcessor import GeneratorPreProcessor
+
+# Validating:
+# ---
+# vm_definition:
+# - name: control-plane
+#   vcpu: 8
+#   memory_mb: 32768
+#   boot_disk_size_gb: 100
+# - name: compute
+#   vcpu: 16
+#   memory_mb: 65536
+#   boot_disk_size_gb: 200
+# - name: storage
+#   vcpu: 10
+#   memory_mb: 32768
+#   boot_disk_size_gb: 100
+#   datastore: Datastore1
+#   network: "VM Network"
+
+
+def preprocessor(attributes=None, fullConfig=None):
+    g = GeneratorPreProcessor(attributes,fullConfig)
+
+    g('name').isRequired()
+    g('vcpu').isRequired()
+    g('memory_mb').isRequired()
+    g('boot_disk_size_gb').isRequired()
+    
+    result = {
+        'attributes_updated': g.getExpandedAttributes(),
+        'errors': g.getErrors()
+    }
+    return result
+
+
