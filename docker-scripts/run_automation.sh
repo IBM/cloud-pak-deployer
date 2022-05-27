16ba61bd7cc704e795d66611f21546a07ea6f49e
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-05-27 07:08:36 +0000

    #511 Licenses must be explicitly accepted

--- docker-scripts/run_automation.sh
+++ docker-scripts/run_automation.sh
@@ -73,6 +73,7 @@ env|environment)
   run_cmd+=" --extra-vars cpd_skip_mirror=${CPD_SKIP_MIRROR}"
   run_cmd+=" --extra-vars cpd_skip_portable_registry=${CPD_SKIP_PORTABLE_REGISTRY}"
   run_cmd+=" --extra-vars cpd_test_cartridges=${CPD_TEST_CARTRIDGES}"
+  run_cmd+=" --extra-vars cpd_accept_licenses=${CPD_ACCEPT_LICENSES}"
 
   if [ ! -z $VAULT_PASSWORD ];then
     run_cmd+=" --extra-vars VAULT_PASSWORD=${VAULT_PASSWORD}"
