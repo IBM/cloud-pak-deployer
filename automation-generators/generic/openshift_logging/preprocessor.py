b3c36e45418c6e8f60685b431a93d5c29fbb074c
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-06-26 15:42:11 +0000

    #29 Add linter for cluster_wide_logging

--- automation-generators/generic/openshift_logging/preprocessor.py
+++ automation-generators/generic/openshift_logging/preprocessor.py
@@ -4,22 +4,36 @@ import sys
 # Validating:
 # ---
 # openshift_logging:
-# - openshift_cluster_name: {{ env_id }}
+# - openshift_cluster_name: "{{ env_id }}"
+#   configure_es_log_store: False
+#   cluster_wide_logging:
+#   - input: application
+#     logging_name: loki-application
+#     labels:
+#       cluster_name: "{{ env_id }}"
+#   - input: infrastructure
+#     logging_name: loki-application
+#     labels:
+#       cluster_name: "{{ env_id }}"
+#   - input: audit
+#     logging_name: loki-audit
+#     labels:
+#       cluster_name: "{{ env_id }}"
 #   logging_output:
 #   - name: loki-application
 #     type: loki
 #     url: https://loki-application.sample.com
 #     certificates:
-#       cert: {{ env_id }}-loki-cert
-#       key: {{ env_id }}-loki-key
-#       ca: {{ env_id }}-loki-ca
+#       cert: "{{ env_id }}"-loki-cert
+#       key: "{{ env_id }}"-loki-key
+#       ca: "{{ env_id }}"-loki-ca
 #   - name: loki-audit
 #     type: loki
 #     url: https://loki-audit.sample.com
 #     certificates:
-#       cert: {{ env_id }}-loki-cert
-#       key: {{ env_id }}-loki-key
-#       ca: {{ env_id }}-loki-ca
+#       cert: "{{ env_id }}"-loki-cert
+#       key: "{{ env_id }}"-loki-key
+#       ca: "{{ env_id }}"-loki-ca
 
 def preprocessor(attributes=None, fullConfig=None):
     g = GeneratorPreProcessor(attributes,fullConfig)
@@ -30,6 +44,23 @@ def preprocessor(attributes=None, fullConfig=None):
     if len(g.getErrors()) == 0:
         fc = g.getFullConfig()
         ge=g.getExpandedAttributes()
+
+        if 'configure_es_log_store' in ge:
+            if type(ge['configure_es_log_store']) != bool:
+                g.appendError(msg='Attribute configure_es_log_store must be either true or false if specified. Default is false.')
+
+        if 'cluster_wide_logging' in ge:
+            for cwl in ge['cluster_wide_logging']:
+                if 'logging_name' in cwl:
+                    if not 'logging_output' in ge:
+                        g.appendError(msg='If logging_name specified for cluster_wide_logging, the logging_output entry must be added with the associated name')
+                    else:
+                        logging_name_found=False
+                        for lo in ge['logging_output']:
+                            if 'name' in lo and lo['name']==cwl['logging_name']:
+                                logging_name_found=True
+                        if not logging_name_found:
+                            g.appendError(msg='logging_output entry with name '+cwl['logging_name']+' was not found')
 
         if 'logging_output' in ge:
             for lo in ge['logging_output']:
