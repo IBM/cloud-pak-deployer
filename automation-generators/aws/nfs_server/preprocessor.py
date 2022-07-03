f89c23bae95bfd8e4ceee7bb84e06d82ad1a07b1
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-07-03 09:10:48 +0000

    #39 Add nfs_server object for AWS

---
+++ automation-generators/aws/nfs_server/preprocessor.py
@@ -0,0 +1,17 @@
+from generatorPreProcessor import GeneratorPreProcessor
+
+def preprocessor(attributes=None, fullConfig=None):
+
+    g = GeneratorPreProcessor(attributes,fullConfig)
+    g('name').isRequired()
+    g('infrastructure').isRequired()
+    g('infrastructure.aws_region').isRequired()
+    g('infrastructure.storage_folder').isRequired()
+
+    result = {
+        'attributes_updated': g.getExpandedAttributes(),
+        'errors': g.getErrors()
+    }
+    return result
+
+
