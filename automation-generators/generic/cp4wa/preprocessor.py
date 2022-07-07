746e9d3902d9a8ddd91e525cee0e4c4e99c5524e
doormat18 <mat.davis@gmail.com>
2022-07-07 15:56:07 -0400

    clean up preprocessor
    Signed-off-by: doormat18 <mat.davis@gmail.com>

--- automation-generators/generic/cp4wa/preprocessor.py
+++ automation-generators/generic/cp4wa/preprocessor.py
@@ -29,189 +29,12 @@ import sys, os
 # Validating:
 # ---
 
-#cp4d:
-#- project: zen-40
+#cp4wa:
+#- project: cp4waiops
 #  openshift_cluster_name: sample
-#  cp4d_version: 4.0
-#  openshift_storage_name: nfs-storage
+#  cp4wa_version: v3.3
 #  use_case_files: True
 #  accept_licenses: False
-#  olm_utils: False
-#  change_node_settings: True
-
-#  cartridges:
-#  - name: cpfs
-#    license_service:
-#      state: disabled
-#      threads_per_core: 2
-#    case_version: 1.10.1
-#  - name: cpd_platform
-#    subscription_channel: v2.0
-#    case_version: 2.0.8
-#  - name: ws
-#    version: 4.0.4
-#    subscription_channel: v2.0
-#    case_version: 2.0.4
-#  - name: wml
-#    version: 4.0.4
-#    subscription_channel: v1.1
-#    case_version: 4.0.5
-#    size: small
-
-#
-# All tested cartridges. To install, uncomment the entry, make sure that the "-"" and properties
-# are aligned with the other "cartridges" entries.
-#
-
-  # - name: analyticsengine 
-  #   version: 4.0.4
-  #   subscription_channel: stable-v1
-  #   case_version: 4.0.4
-  #   size: small 
-  # - name: bigsql
-  #   version: 7.2.3
-  #   subscription_channel: v7.2
-  #   case_version: 7.2.3
-  # - name: cognos_analytics
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.6
-  #   size: small
-  #   instances:
-  #   - name: ca-instance
-  #     metastore_ref: ca-metastore
-  # - name: cde
-  #   version: 4.0.4
-  #   subscription_channel: v1.0
-  #   case_version: 2.0.4
-  # - name: datagate
-  #   version: 2.0.4
-  #   subscription_channel: v2.0
-  #   case_version: 4.0.4
-  # - name: datastage
-  #   version: 4.0.4
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.5
-  # - name: db2
-  #   version: 4.0.6
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.6
-  #   size: small
-  #   instances:
-  #   - name: ca-metastore
-  #     metadata_size_gb: 20
-  #     data_size_gb: 20
-  #     backup_size_gb: 20  
-  #     transactionlog_size_gb: 20
-  # - name: db2u
-  #   version: 4.0.6
-  #   subscription_channel: v1.1
-  #   case_version: 4.0.6
-  # - name: db2wh
-  #   version: 4.0.6
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.6
-  # - name: dmc
-  #   version: 4.0.3
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.3
-  # - name: dods
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.4
-  #   size: small
-  # - name: dp
-  #   version: 4.0.4
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.4
-  #   size: small
-  # - name: dv
-  #   version: 1.7.3
-  #   subscription_channel: v1.7
-  #   case_version: 1.7.3
-  #   size: small 
-  #   dependencies:
-  #   - name: db2u
-  #   instances:
-  #   - name: data-virtualization
-  # - name: hee
-  #   version: 4.0.4
-  #   size: small
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.4
-  # - name: mdm
-  #   version: 1.1.167
-  #   subscription_channel: v1.1
-  #   case_version: 1.0.166
-  #   size: small
-  #   wkc_enabled: true
-  # - name: openpages
-  #   version: 8.204.1
-  #   subscription_channel: v1.0
-  #   case_version: 2.1.1+20211213.164652.82041218
-  # - name: planning-analytics
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.40403
-  # - name: rstudio
-  #   version: 4.0.4
-  #   subscription_channel: v1.0
-  #   case_version: 1.0.4
-  #   size: small
-  # - name: spss
-  #   version: 4.0.4
-  #   subscription_channel: v1.0
-  #   case_version: 1.0.4
-  # - name: voice-gateway
-  #   version: 1.0.7
-  #   subscription_channel: v1.0
-  #   case_version: 1.0.4
-  #   replicas: 1  
-  # - name: watson_assistant
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.4
-  #   size: small
-  # - name: watson-discovery
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.4
-  # - name: watson-ks
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.4
-  #   size: small
-  # - name: watson-openscale
-  #   version: 4.0.4
-  #   subscription_channel: v1
-  #   case_version: 2.4.0
-  #   size: small
-  # - name: watson-speech
-  #   version: 4.0.4
-  #   subscription_channel: v4.0
-  #   case_version: 4.0.4
-  #   stt_size: xsmall
-  #   tts_size: xsmall
-  # - name: wkc
-  #   version: 4.0.4
-  #   subscription_channel: v1.0
-  #   case_version: 4.0.4
-  #   size: small
-  # - name: wml
-  #   version: 4.0.4
-  #   subscription_channel: v1.1
-  #   case_version: 4.0.5
-  #   size: small
-  # - name: wml-accelerator
-  #   version: 2.3.4
-  #   subscription_channel: v1.0
-  #   case_version: 2.3.4
-  #   replicas: 1
-  #   size: small
-  # - name: ws
-  #   version: 4.0.4
-  #   subscription_channel: v2.0
-  #   case_version: 2.0.4
 
 def str_to_bool(s):
     if s == None:
@@ -228,7 +51,6 @@ def preprocessor(attributes=None, fullConfig=None):
     openshift_cluster_name=g('openshift_cluster_name').getExpandedAttributes()['openshift_cluster_name']
     g('cp4wa_version').isRequired()
     g('openshift_storage_name').expandWithSub('openshift', remoteIdentifier='name', remoteValue=openshift_cluster_name, listName='openshift_storage',listIdentifier='storage_name')
-    g('olm_utils').isOptional()
     g('use_case_files').isOptional()
     g('accept_licenses').isOptional()
 
@@ -236,10 +58,6 @@ def preprocessor(attributes=None, fullConfig=None):
     if len(g.getErrors()) == 0:
         fc = g.getFullConfig()
         ge=g.getExpandedAttributes()
-
-        # Check for cp4d:     
-        # Check that cpfs element exists
-        # Check that cpd_platform element exists
 
         # Check accept_licenses property
         if 'accept_licenses' in ge:
@@ -251,16 +69,6 @@ def preprocessor(attributes=None, fullConfig=None):
             if not accept_licenses:
                 if not str_to_bool(os.environ.get('CPD_ACCEPT_LICENSES')):
                     g.appendError(msg="You must accept licenses by specifying accept_licenses: True or by using the --accept-all-licenses command line flag")
-
-        # Store olm_utils property
-        if 'olm_utils' in ge:
-            olm_utils=ge['olm_utils']
-        else:
-            olm_utils=False
-        # Check if olm utils is installed
-        if olm_utils:
-            if not os.path.exists(os.path.expanduser('~')+'/bin/apply-olm'):
-                g.appendError(msg="Container image was not built with olm-utils, cannot specify olm_utils: True")
 
 # Check reference
 # - Retrieve the openshift element with name=openshift_cluster_name
