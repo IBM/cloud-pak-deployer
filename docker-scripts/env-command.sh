45c7b4d8fe299cc644c4ac728305d75dbf456dec
Fei Ye <feiye@cn.ibm.com>
2022-10-14 15:58:18 +0800

    Generate SSH key pair if not in vault already
    Signed-off-by: Fei Ye <feiye@cn.ibm.com>

--- docker-scripts/env-command.sh
+++ docker-scripts/env-command.sh
@@ -8,6 +8,35 @@ echo "--------------------------------------------------------------------------
 
 export PS1='\[\e]0;\w\a\]\n[\#] \[\e[32m\u@Cloud Pak Deployer:\[\e[33m\]\w \e[m\$ ';
 
+if [ -z "$cloud_platform" ]; then
+  echo "cloud_platform is not defined."
+  exit 1
+fi
+
+#Create the ssh key pair
+if [ -f "$STATUS_DIR/vault/$cloud_platform" ]; then
+  ocp_ssh_private_key=$(awk -F "-key=" '/ocp-ssh-private/ {print $2}' $STATUS_DIR/vault/$cloud_platform)
+  ocp_ssh_pub_key=$(awk -F "-key=" '/ocp-ssh-pub/ {print $2}' $STATUS_DIR/vault/$cloud_platform)
+
+  if [ ! -z "$ocp_ssh_private_key" ] && [ ! -z "$ocp_ssh_pub_key" ];then
+  ssh_key_pair_folder='/root/.ssh'
+  if [ ! -d "$ssh_key_pair_folder" ]; then
+    mkdir -p "$ssh_key_pair_folder"
+  fi
+  echo "Create the SSH key-pair id_rsa.pub and id_rsa files in the folder root/.ssh/"
+  echo $ocp_ssh_private_key |base64 -d > "$ssh_key_pair_folder"/id_rsa
+  echo $ocp_ssh_pub_key |base64 -d > "$ssh_key_pair_folder"/id_rsa.pub
+  #fix the issue "invalid format"
+  sed -i '$a\\r' "$ssh_key_pair_folder"/id_rsa
+  chmod 600 "$ssh_key_pair_folder"/id_rsa
+  chmod 600 "$ssh_key_pair_folder"/id_rsa.pub
+fi
+
+fi
+
+
+
+
 oc_zip=$(find $STATUS_DIR/downloads/ -name "openshift-client*" | tail -1)
 if [ "$oc_zip" != "" ];then
   echo "Installing OpenShift client"
