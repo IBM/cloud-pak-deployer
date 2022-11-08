a1258b3771c0a272029b075413ee39471d7cf3c8
Frank Ketelaars <fketelaars@nl.ibm.com>
2022-11-08 22:47:05 +0100

    #209 Documentation build fix

--- doc/run-npm-dev.sh
+++ doc/run-npm-dev.sh
@@ -4,11 +4,15 @@ source /root/.bashrc
 
 cd /doc
 
+echo "Cleaning up old artifacts..."
+rm -rf node_modules .cache package-lock.json public
+
 echo "Installing the required Node.js modules..."
+npm i apollo-boost graphql react-apollo -S
 npm install
 
 echo "Installing Gatsby..."
-npm install -g gatsby-cli
+npm install -g gatsby-cli@4
 
 echo "Starting the rendering process..."
 npm run dev
