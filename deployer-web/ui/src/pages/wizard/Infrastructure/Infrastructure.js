f7eb9c2eae0cbea59ace7ba36630047a366beeee
Fei Ye <feiye@cn.ibm.com>
2022-11-08 23:16:55 +0800

    Enable Existing OpenShift, AWS and Move CP entitlement key input field to the Cloud Pak page
    Signed-off-by: Fei Ye <feiye@cn.ibm.com>

--- deployer-web/ui/src/pages/wizard/Infrastructure/Infrastructure.js
+++ deployer-web/ui/src/pages/wizard/Infrastructure/Infrastructure.js
@@ -3,14 +3,19 @@ import { InlineNotification, Loading, RadioButton, RadioButtonGroup, TextInput }
 import { useEffect, useState } from "react";
 import './Infrastructure.scss'
 
-const Infrastructure = ({cloudPlatform, IBMCloudSettings, updateInfraValue, updateWizardError, setIBMCloudSettings}) => {
+const Infrastructure = ({cloudPlatform, IBMCloudSettings, updateInfraValue, updateWizardError, setIBMCloudSettings, AWSSettings, setAWSSettings}) => {
 
+    //IBM Cloud
     const [loadingIBMRegion, setLoadingIBMRegion] = useState(false)
     const [loadIBMRegionErr, setLoadIBMRegionErr] = useState(false)
     const [IBMRegion, setIBMRegion] = useState(IBMCloudSettings.region)
-    const [AWSRegion, setAWSRegion] = useState(IBMCloudSettings.region)
+    const [isIBMregionInValid, setIBMregionInValid] = useState(false)
 
-    const [isIBMregionInValid, setIBMregionInValid] = useState(false)    
+    //AWS
+    const [AWSAccessKeyID, setAWSAccessKeyID] = useState(AWSSettings.accessKeyID)
+    const [AWSSecretAccessKey, setAWSSecretAccessKey] = useState(AWSSettings.secretAccessKey)
+    const [AWSRegion, setAWSRegion] = useState(AWSSettings.region)    
+    const [isAWSregionInValid, setAWSregionInValid] = useState(false)      
 
     useEffect(() => {
       const getIBMRegion = async() => {
@@ -23,13 +28,14 @@ const Infrastructure = ({cloudPlatform, IBMCloudSettings, updateInfraValue, upda
         });
         setLoadingIBMRegion(false)  
       }
-      if (cloudPlatform === 'ibm-cloud') {        
+      
+      if (cloudPlatform === 'ibm-cloud') {           
         if (IBMRegion === '') {
           setLoadingIBMRegion(true)
           getIBMRegion()
         }
       }      
-    }, [])
+    }, [cloudPlatform])
 
     const setCloudPlatformValue = (value) => {    
       updateInfraValue({cloudPlatform: value});
@@ -71,6 +77,69 @@ const Infrastructure = ({cloudPlatform, IBMCloudSettings, updateInfraValue, upda
       hideCloseButton: false,
     });  
 
+    // 0 - IBMCloudPlatform
+    const IBMCloudPlatform = () => {
+      return (        
+        <>
+          {loadingIBMRegion && <Loading /> }          
+          <div className="infra-container">
+            <div>
+              <div className="infra-items">IBM Cloud API Key</div>
+              <TextInput.PasswordInput onChange={setIBMAPIKeyValue} placeholder="IBM Cloud API Key" id="0" labelText="" value={IBMCloudSettings.IBMAPIKey} />
+            </div>
+            {/* <div>
+              <div className="infra-items">Entitlement key</div>
+              <TextInput.PasswordInput onChange={setEntilementKeyValue} placeholder="Entitlement key" id="1" labelText="" value={IBMCloudSettings.entilementKey}/>
+            </div> */}
+            <div>
+              <div className="infra-items">Enviroment ID</div>
+              <TextInput onChange={setEnvIDValue} placeholder="Enviroment ID" id="2" labelText="" value={IBMCloudSettings.envId} />
+            </div>  
+            <div>
+              <div className="infra-items">IBM Cloud Region</div>
+              <TextInput onChange={setIBMCloudRegion} placeholder={IBMRegion} id="3" labelText="" value={IBMRegion} invalidText="IBM Cloud Region can not be empty."  invalid={isIBMregionInValid}/>
+            </div> 
+          </div>        
+        </> 
+      );
+    }
+
+    // 1 - AWSPlatform
+    const AWSPlatform = () => {
+      return (
+        <>
+          <div className="infra-container">
+            <div>
+              <div className="infra-items">AWS Access Key ID</div>
+              <TextInput placeholder="AWS Access Key" id="10" labelText="" value={AWSSettings.AWSAccessKeyID} />
+            </div>
+            <div>
+              <div className="infra-items">AWS Secret Access Key</div>
+              <TextInput.PasswordInput placeholder="AWS Secret Access Key" id="11" labelText="" value={AWSSettings.AWSSecretAccessKey}/>
+            </div>
+            <div>
+              <div className="infra-items">AWS Region</div>
+              <TextInput onChange={setIBMCloudRegion} placeholder={AWSRegion} id="12" labelText="" value={AWSRegion} invalidText="AWS region."  invalid={isAWSregionInValid}/>
+            </div> 
+          </div>
+        </>
+      )
+    }
+
+    // 3 - ExistingOpenShiftPlatform
+    const ExistingOpenShiftPlatform = () => {
+      return (
+        <>
+          <div className="infra-container">
+            <div>
+              <div className="infra-items">oc login command</div>
+              <TextInput placeholder="oc login command" id="30" labelText="" />
+            </div>
+          </div>
+        </>
+      )
+    }    
+
     return (
       <> 
       <div className="infra-title">Cloud Platform</div>  
@@ -84,63 +153,14 @@ const Infrastructure = ({cloudPlatform, IBMCloudSettings, updateInfraValue, upda
          }
          >
          <RadioButton labelText="IBM Cloud" value="ibm-cloud" id="0" />
-         <RadioButton labelText="AWS" value="aws" id="1" disabled  />
+         <RadioButton labelText="AWS" value="aws" id="1" />
          <RadioButton labelText="vSphere" value="vsphere" id="2" disabled />
          <RadioButton labelText="Existing OpenShift" value="existing-ocp" id="3" />
       </RadioButtonGroup>
 
-      {cloudPlatform === 'ibm-cloud' ? 
-        <>
-          {loadingIBMRegion && <Loading /> } 
-         
-          <div className="infra-container">
-            <div>
-              <div className="infra-items">IBM Cloud API Key</div>
-              <TextInput.PasswordInput onChange={setIBMAPIKeyValue} placeholder="IBM Cloud API Key" id="0" labelText="" value={IBMCloudSettings.IBMAPIKey} />
-            </div>
-            <div>
-              <div className="infra-items">Entitlement key</div>
-              <TextInput.PasswordInput onChange={setEntilementKeyValue} placeholder="Entitlement key" id="1" labelText="" value={IBMCloudSettings.entilementKey}/>
-            </div>
-            <div>
-              <div className="infra-items">Enviroment ID</div>
-              <TextInput onChange={setEnvIDValue} placeholder="Enviroment ID" id="2" labelText="" value={IBMCloudSettings.envId} />
-            </div>  
-            <div>
-              <div className="infra-items">IBM Cloud Region</div>
-              <TextInput onChange={setIBMCloudRegion} placeholder={IBMRegion} id="3" labelText="" value={IBMRegion} invalidText="IBM Cloud Region can not be empty."  invalid={isIBMregionInValid}/>
-            </div> 
-          </div>        
-        </> 
-          : null}
-
-      {cloudPlatform === 'aws' ?
-        <>
-          <div className="infra-container">
-            <div>
-              <div className="infra-items">AWS Access Key</div>
-              <TextInput placeholder="AWS Access Key" id="4" labelText="" />
-            </div>
-            <div>
-              <div className="infra-items">AWS Secret Access Key</div>
-              <TextInput.PasswordInput placeholder="AWS Secret Access Key" id="4" labelText="" />
-            </div>
-            {/* <div>
-              <div className="infra-items">AWS Region</div>
-              <TextInput onChange={setIBMCloudRegion} placeholder={AWSRegion} id="3" labelText="" value={AWSRegion} invalidText="AWS region."  invalid={isAWSregionInValid}/>
-            </div>  */}
-          </div>
-        </> : null}
-
-        {cloudPlatform === 'existing-ocp' ?
-        <>
-          <div className="infra-container">
-            <div>
-              <div className="infra-items">oc login command</div>
-              <TextInput placeholder="oc login command" id="4" labelText="" />
-            </div>
-          </div>
-        </> : null}
+      {cloudPlatform === 'ibm-cloud' ?  <IBMCloudPlatform /> : null } 
+      {cloudPlatform === 'aws' ? <AWSPlatform />: null}
+      {cloudPlatform === 'existing-ocp' ? <ExistingOpenShiftPlatform/> : null}
 
       </>
     );
