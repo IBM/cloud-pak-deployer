bf6ab24d480f6ec2cdc3ca90a28b0a8fb7169038
Frank Ketelaars <fketelaars@nl.ibm.com>
2024-02-12 22:21:56 +0000

    #580 Deploy Red Hat SSO

---
+++ automation-generators/generic/openshift_redhat_sso/preprocessor.py
@@ -0,0 +1,18 @@
+from generatorPreProcessor import GeneratorPreProcessor
+import sys
+
+# Validating:
+# ---
+# openshift_sso:
+# - openshift_cluster_name: {{ env_id }}
+
+def preprocessor(attributes=None, fullConfig=None, moduleVariables=None):
+    g = GeneratorPreProcessor(attributes,fullConfig,moduleVariables)
+
+    g('openshift_cluster_name').isRequired()
+
+    result = {
+        'attributes_updated': g.getExpandedAttributes(),
+        'errors': g.getErrors()
+    }
+    return result
