5bf65d8cdd3bb92492670622273a5f3e33165b5a
Frank Ketelaars <fketelaars@nl.ibm.com>
2024-03-03 19:58:38 +0000

    #580 Auto-expand cp4d project and openshift cluster

--- automation-generators/generic/cp4d_role/preprocessor.py
+++ automation-generators/generic/cp4d_role/preprocessor.py
@@ -17,16 +17,19 @@ import sys
 def preprocessor(attributes=None, fullConfig=None, moduleVariables=None):
     g = GeneratorPreProcessor(attributes,fullConfig,moduleVariables)
 
-    g('openshift_cluster_name').isRequired()
-    g('project').isRequired()
+    g('openshift_cluster_name').expandWith('openshift[*]',remoteIdentifier='name')
+    g('project').expandWith('cp4d[*]',remoteIdentifier='project')
     g('name').isRequired()
-    g('state').isRequired()
+    g('state').mustBeOneOf(['installed', 'removed'])
     g('description').isRequired()
     g('permissions').isRequired()
 
     # Now that we have reached this point, we can check the attribute details if the previous checks passed
     if len(g.getErrors()) == 0:
         ge=g.getExpandedAttributes()
+
+        if not (isinstance(ge['permissions'], list)):
+            g.appendError(msg="Every role must at least have 1 permission")
 
         if ge['state'] not in ['installed','removed']:
             g.appendError(msg='cp4d_role state must be "installed" or "removed"')
