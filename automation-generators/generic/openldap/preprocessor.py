cabdaed111b2f267ff32d9b39422b963fd5c6929
Frank Ketelaars <fketelaars@nl.ibm.com>
2024-03-20 07:11:23 +0000

    #580 Add openldap configuration

--- automation-generators/generic/openldap/preprocessor.py
+++ automation-generators/generic/openldap/preprocessor.py
@@ -10,6 +10,7 @@ def preprocessor(attributes=None, fullConfig=None, moduleVariables=None):
     g = GeneratorPreProcessor(attributes,fullConfig,moduleVariables)
 
     g('openshift_cluster_name').expandWith('openshift[*]',remoteIdentifier='name')
+    g('state').isRequired()
 
     result = {
         'attributes_updated': g.getExpandedAttributes(),
