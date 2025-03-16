ae18e9d5c13ef6db04502c51b8b34022ba4cc7b9
Frank Ketelaars <fketelaars@nl.ibm.com>
2025-03-16 14:40:39 +0000

    #918 Add support for LDAPs
    Signed-off-by: Frank Ketelaars <fketelaars@nl.ibm.com>

--- automation-generators/generic/ldap/preprocessor.py
+++ automation-generators/generic/ldap/preprocessor.py
@@ -22,21 +22,37 @@ def preprocessor(attributes=None, fullConfig=None, moduleVariables=None):
     g('project').expandWith('cp4d[*]',remoteIdentifier='project')
     g('ldap_url').isRequired()
     g('ldap_base_dn').isRequired()
-    g('ldap_bind_dn').isRequired()
-    g('ldap_bind_password_vault_secret').isRequired()
+    g('ldap_bind_dn').isOptional()
+    g('ldap_bind_password_vault_secret').isOptional()
     g('ldap_group_filter').isRequired()
     g('ldap_group_id_map').isRequired()
     g('ldap_group_member_id_map').isRequired()
     g('ldap_user_filter').isRequired()
     g('ldap_user_id_map').isRequired()
+    g('ldap_tls_verify_client').isOptional().mustBeOneOf(['demand', 'try'])
+    g('ldap_tls_client_cert_vault_secret').isOptional()
+    g('ldap_tls_client_key_vault_secret').isOptional()
 
     if len(g.getErrors()) == 0:
         ge=g.getExpandedAttributes()
+
+        if "ldap_tls_client_cert_vault_secret" in ge and "ldap_tls_client_key_vault_secret" not in ge or \
+            "ldap_tls_client_key_vault_secret" in ge and "ldap_tls_client_cert_vault_secret" not in ge:
+            g.appendError(msg='Both ldap_tls_client_cert_vault_secret and ldap_tls_client_key_vault_secret attributes must be set, or none')
+
+        if ("ldap_tls_client_cert_vault_secret" in ge or "ldap_tls_client_key_vault_secret" in ge) and \
+            "ldap_tls_verify_client" not in ge:
+            g.appendError(msg='If ldap_tls_client_cert_vault_secret and ldap_tls_client_key_vault_secret attributes specified, ldap_tls_verify_client must also be specified')
 
         if "ldap_case_insensitive" in ge:
             if type(ge['ldap_case_insensitive']) != bool:
                 g.appendError(msg='Attribute ldap_case_insensitive must be either True or False if specified. Default is False.')
 
+        if "ldap_bind_dn" in ge and "ldap_bind_password_vault_secret" not in ge:
+            g.appendError(msg='If attribute ldap_bind_dn is specified, the ldap_bind_password_vault_secret must also be set.')
+
+        if "ldap_bind_password_vault_secret" in ge and "ldap_bind_dn" not in ge:
+            g.appendError(msg='If attribute ldap_bind_password_vault_secret is specified, the ldap_bind_dn must also be set.')
 
     result = {
         'attributes_updated': g.getExpandedAttributes(),
